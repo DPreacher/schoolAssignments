5334c6554ea162cfce7ccf0d2557bf42cfb4a4a4
DPreacher <DPreacher@users.noreply.github.com>
2015-11-03 14:14:15 -0500

    Create 3November2015.sql

---
+++ StLawrence/semester3/Database/Lessons/3November2015.sql
@@ -0,0 +1,74 @@
+DROP DATABASE IF EXISTS tickets;
+
+CREATE DATABASE tickets;
+
+USE tickets;
+
+CREATE TABLE event
+(
+    eventID SERIAL PRIMARY KEY,
+    date DATE,
+    eventType ENUM('CONCERT', 'SPORTS')
+);
+
+CREATE TABLE customer
+(
+    customerID SERIAL PRIMARY KEY,
+    firstName VARCHAR(20),
+    LastName VARCHAR(20)
+);
+
+CREATE TABLE customerEvent
+(
+    customerID BIGINT,
+    eventID BIGINT,
+    ticketCount INT DEFAULT 0
+);
+    /*  no primary key set yet
+    Point:
+        ask for ticket count at the time of purchase
+*/
+CREATE TABLE ticket
+(
+    ticketID SERIAL PRIMARY KEY,
+    eventID     BIGINT
+);
+
+/* Excercise
+    Add in Foriegn keys and  
+... */
+
+INSERT INTO event VALUES
+(NULL, '2015-11-20','SPORTS'),
+(null, '2015-11-23', 'CONCERT');
+
+insert into customer values
+(null, 'fred', 'smith'),
+(null, 'sue', 'taylor');
+
+DELIMITER // /* 2 common dilimiters is 2 forward slashes or 2 dollar symbols */
+
+CREATE PROCEDURE SellTicket
+    /* PERAMETER*/
+    ( IN customer BIGINT,
+      /* NEED TO SPECIFIY the direction the data is going with the procedure ( ex: IN)*/
+      IN event BIGINT, /* references eventID.  The name doen't have to be the same*/
+      IN nTickets INT
+    )
+BEGIN /* Start the procedure */
+    IF nTickets <= 4 THEN
+        INSERT INTO customerEvent VALUES
+        (customer, event, nTickets);
+    ELSE
+        SELECT 'Too Many Tickets';
+    END IF;
+END//
+DELIMITER ; /* Set the delimiter back to the semi-colon*/
+
+/* call the above procedure */
+call sellTicket(1,1,4);
+call sellTicket(1,2,4);
+call sellTicket(2,2,7);
+
+/* check the customer Event log */
+SELECT * FROM customerEvent;
