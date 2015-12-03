6ce0cb6ccb936a17555d2b71b2cb419b37c1c595
DPreacher <DPreacher@users.noreply.github.com>
2015-12-03 11:08:58 -0500

    Update employees.js

--- StLawrence/semester3/comp206/Assignment3/employees.js
+++ StLawrence/semester3/comp206/Assignment3/employees.js
@@ -1,21 +1,126 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-var http_request=new XMLHttpRequest();
-$(document).ready(
-    function(){
-    setupDept();
+/* id = this.id;
+        $.ajax({
+            url: "populateEmpl.php", 
+            data: {"deptId": id,"index":index},
+            type: "post",
+            datatype:"json",
+            error:  function(xhr,status,error){
+                    alert(status+" "+error);
+            },
+            success: function (result){
+                populateTable(result);
+           }
+		   
+		   
+function getIndex(){
     
+    $.ajax({
+       url: "getSession.php",
+       data: {"option":option},
+       datatype: "json",
+       type: "post",
+       error:  function(xhr,status,error){
+                alert(status+" "+error);
+        },
+        success: function (result){
+            
+            //var obj = JSON.parse(result);
+            var obj = JSON.parse(result);
+            maxIndex= obj[0].emplIndex/10;
+            index=0;
+            ajaxCall(0);
+        }
+    });
+}*/
+var id="", index=0, maxIndex=0;
+$("document").ready(function(){
+    
+    $(".deptRow td").click(function(){
+        id=this.id;
+        index=0;
+        getIndex();
+        // ajaxCall();
+   
+    });
+    $(".prev").click(function(){
+            alert(works);
+            callAjax(-10);
+       
+    });
+    $(".next").click(function(){
+            alert(works);
+            callAjax(10);
+       
+    });
 });
-
-function setupDept(){
-        http_request.onreadystatechange = handleResponse;
-        http_request.open("GET","deptTable.php",true);
-        http_request.send();
+function getIndex(){
+    
+    $.ajax({
+       url: "getSession.php",
+       data: {"deptId":id},
+       datatype: "json",
+       type: "post",
+       error:  function(xhr,status,error){
+                alert(status+" "+error);
+        },
+        success: function (result){
+            
+            //var obj = JSON.parse(result);
+            var obj = JSON.parse(result);
+            maxIndex= obj[0].emplIndex/10;
+            index=0;
+            ajaxCall(0);
+        }
+    });
 }
-function handleResponse(){
-    if(http_request.readyState==4&&http_request.status==200)
-        $("#conent1").html(http_request.responseText);
+function ajaxCall(move){
+    $.ajax({
+        url: "populateEmpl.php", 
+        data: {"move":move},
+        type: "post",
+        datatype:"json",
+        error:  function(xhr,status,error){
+                alert(status+" "+error);
+        },
+        success: function (result){
+            populateTable(result,move);
+        }
+    });
+}
+
+function populateTable(result, move){
+        dept = document.getElementById(id);
+        var empTbl=document.getElementById("empl");
+        empTbl.innerHTML="<table id='emplTable'></table>";
+        rowHTML="<tr id='emplData'></tr><tr><th colspan='2'>"
+        rowHTML+=dept.innerHTML;
+        rowHTML+="</th>";
+         $("#emplTable").append(rowHTML);
+         
+        rowHTML="<tr><td><button type='button' id='prev'disabled>Previous</button></td>";
+        rowHTML+="<td><button type='button' id='next'>Next</button></td>";
+        $("#emplTable").append(rowHTML);
+        
+        var obj = JSON.parse(result);
+        for(row=0;row<result.length;row++){
+
+            rowHTML="<tr><td class='emplRow'>";
+            rowHTML+=obj[row].first_name;
+            rowHTML+="</td><td class='emplRow'>";
+            rowHTML+=obj[row].last_name;
+            rowHTML+="</td></tr>";
+            $("#emplTable").append(rowHTML);
+        }
+		index+=move;
+        if(move==0||index<=0){
+            index==0;
+            document.getElementById("prev").disabled=true;
+        }
+        else if(index>=MaxIndex-10){
+            document.getElementById("next").disabled=true;
+        }
+        else{
+            document.getElementById("prev").disabled=false;
+            document.getElementById("next").disabled=false;
+        }
 }
