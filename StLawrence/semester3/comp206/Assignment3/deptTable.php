2e2ce5e5bb7c44665af4c6f445da14c233257b94
DPreacher <DPreacher@users.noreply.github.com>
2015-12-03 11:09:41 -0500

    Update deptTable.php

--- StLawrence/semester3/comp206/Assignment3/deptTable.php
+++ StLawrence/semester3/comp206/Assignment3/deptTable.php
@@ -1,20 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 $connection=  mysqli_connect("localhost", "root", "mysql", "employees");
 $departments =mysqli_query($connection,"SELECT * FROM departments;");
+
 echo "<table id='deptTable'>";
 echo "<tr><th>Departments</th></tr>";
 while($row=  mysqli_fetch_assoc($departments)){
-    
+
     $id=$row["dept_no"];
     $dept=$row["dept_name"];
-    echo "<tr class='deptRow'><td id='$id'>$dept</td></tr>";
-
+    echo "<tr class='deptRow' ><td id='$id'>$dept</td></tr>";
 }
 echo"</table>";
+mysqli_close($connection);
