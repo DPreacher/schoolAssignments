15ceb25a9c7b52f1841f06bcfa956db1797fb412
DPreacher <DPreacher@users.noreply.github.com>
2015-12-04 11:17:07 -0500

    Update populateEmpl.php

--- StLawrence/semester3/comp206/Assignment3/populateEmpl.php
+++ StLawrence/semester3/comp206/Assignment3/populateEmpl.php
@@ -1,28 +1,25 @@
 <?php
-    $session_start();
-    $deptId =$_SESSION["deptID"];
-    $index=$_SESSION["emplIndex"];
+    session_start();
+    $deptId=$_SESSION["id"] ;
+    $index = $_SESSION["index"];
     $move=$_POST["move"];
-    
-    if($move==0)
+    if($move==0||$index+$move<0){
         $index=0;
+        
+    }        
     else{
-        $index=$index+$move;
-        if($index>=$_SESSION["emplMax"]-10)
-            $index=$_SESSION["emplMax"];
-        elseif ($index<0)
-            $index=0;
+        $index+=$move;
     }
-    
+    $_SESSION["index"]=$index;
+        
+        
     $connection=  mysqli_connect("localhost", "root", "mysql", "employees");
-    
-    $sql = "SELECT first_name, last_name FROM dept_emp JOIN employees USING(emp_no) JOIN departments USING(dept_no) WHERE dept_no = '$deptId' LIMIT 10;";
+   
+    $sql = "SELECT first_name, last_name FROM dept_emp JOIN employees USING(emp_no) JOIN departments USING(dept_no) WHERE dept_no = '$deptId' LIMIT $index, 10;";
     $result =mysqli_query($connection,$sql);                                
     
     $rows=Array();
     while($row=  mysqli_fetch_assoc($result)){
             $rows[]=$row;
     }
-    mysqli_close($connection);
-    $_SESSION["emplIndex="]=$index;
     echo json_encode($rows);
