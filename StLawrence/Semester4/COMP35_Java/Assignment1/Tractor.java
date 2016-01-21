c0c2c03b12befbf139a3acc45e05f7d67d7fec01
DPreacher <DPreacher@users.noreply.github.com>
2016-01-21 14:59:56 -0500

    Update Tractor.java

--- StLawrence/Semester4/COMP35_Java/Assignment1/Tractor.java
+++ StLawrence/Semester4/COMP35_Java/Assignment1/Tractor.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ingrama1.Model;
+package Model;
 
 /**
  *
- * @author Daniel
+ * @author dingram20
  */
 public class Tractor {
     int engineRPM;
@@ -16,23 +16,29 @@ public class Tractor {
     private void setRPM() {
         this.engineRPM=24*this.throttleControl;
     }
+
+ 
     public enum Gear{NUETRAL,LOW,MID,HIGH}
     Gear gear;
 
+    public void setGear(Gear gear) {
+        this.gear = gear;
+    }
+    
     public Tractor() {
         this.throttleControl=0;
         this.gear=Gear.NUETRAL;
         this.wheelCircumference=4;
-        this.engineRPM=-1;
+        this.engineRPM=0;
     }
-    public void startEninge(){
+    public void startEngine(){
         if(this.throttleControl>10&&this.gear==Gear.NUETRAL){
            setRPM();
             
         }
     }
-    public void stopEnigne(){
-        this.engineRPM=-1;
+    public void stopEngine(){
+        this.engineRPM=0;
     }
     private int throttleControl;
 
@@ -41,25 +47,38 @@ public class Tractor {
      *
      * @param throttleControl new value of throttleControl
      */
-    public void setThrottleControl(int throttleControl) {
+    public void setThrottle(int throttleControl) {
         //Validate throttle Control
         if(throttleControl >=0&&throttleControl<=100){
             this.throttleControl = throttleControl;
             //check engine on and, if not on, set rpm 
-            if(this.engineRPM!=-1){
+            if(this.engineRPM!=0){
                 this.setRPM();
             }
         }
     }
-
+    private double calculateSpeed(){
+        double gearRatio,speed=0.0;
+        
+        switch(this.gear){
+            case LOW: gearRatio=5.0;break;
+            case MID: gearRatio=10.;break;
+            case HIGH: gearRatio=20.0;break;
+            default: gearRatio=0.0;
+        }
+        if(gearRatio!=0.0)
+            speed =(this.engineRPM/gearRatio*this.wheelCircumference*60)/1000 ;
+        return speed;
+    }
     @Override
     public String toString() {
         String started;
-        if(this.engineRPM==-1)
+        if(this.engineRPM==0)
             started="not started";
         else
-            started="is started";
-        return "Tractor: "+started +", throttle ";/* 
-    }engineRPM: " + engineRPM + ", wheelCircumference=" + wheelCircumference + ", gear=" + gear + ", throttleControl=" + throttleControl + '}';
-    */
+            started="started";
+        
+        return "Tractor: "+started +", throttle= "+this.throttleControl+", engine rpm= "+this.engineRPM+"kpm, tractor speed = "+calculateSpeed()+", gear = "+this.gear;/* 
+    }
+    }
 }
