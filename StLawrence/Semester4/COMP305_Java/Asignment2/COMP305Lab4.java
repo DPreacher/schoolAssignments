ff32931fcb15103e8e51859d0352762bfb0745d2
DPreacher <DPreacher@users.noreply.github.com>
2016-02-22 21:29:01 -0500

    Create COMP305Lab4.java

---
+++ StLawrence/Semester4/COMP305_Java/Asignment2/COMP305Lab4.java
@@ -0,0 +1,27 @@
+
+package comp305lab4;
+
+import javax.swing.JFrame;
+import view.AppFrame;
+
+/**
+ *
+ * @author cbanger
+ */
+public class COMP305Lab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        AppFrame frame = new AppFrame();
+        frame.setVisible(true);
+        frame.setSize(600, 450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+    }
+    
+}
