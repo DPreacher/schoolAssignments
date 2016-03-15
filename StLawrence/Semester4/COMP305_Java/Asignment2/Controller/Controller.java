1cce4980ffb95716977eaf84e5f43781b3f4a24d
DPreacher <DPreacher@users.noreply.github.com>
2016-03-15 11:00:29 -0400

    Update Controller.java

--- StLawrence/Semester4/COMP305_Java/Asignment2/Controller/Controller.java
+++ StLawrence/Semester4/COMP305_Java/Asignment2/Controller/Controller.java
@@ -19,96 +19,124 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
 import java.awt.event.WindowListener;
 import java.awt.event.WindowStateListener;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import model.DrawingFileFilter;
 import model.DrawingModel;
 import model.LineStrokeModel;
 import view.View;
 
 /**
- * 
+ *
  * @author dingram20
  */
-public class Controller 
-implements MouseListener, MouseMotionListener, 
-        ActionListener, ComponentListener, WindowListener,WindowFocusListener, WindowStateListener {
-    int resized =0;
+public class Controller
+        implements MouseListener, MouseMotionListener,
+        ActionListener, ComponentListener, WindowListener, WindowFocusListener, WindowStateListener {
+
+    int resized = 0;
     View view;
     LineStrokeModel stroke;
     DrawingModel drawingModel;
-    
-     Color currentColour=Color.BLACK;
-    int currentThickness=1;
-    /** Controller
-     * 
-     * @param view 
+    boolean drawingCreated=false;
+
+    Color currentColour = Color.BLACK;
+    int currentThickness = 1;
+
+    /**
+     * Controller
+     *
+     * @param view
      * @param drawingModel
-     * @param lineModel 
+     * @param lineModel
      */
-    public Controller(View view,DrawingModel drawingModel) {
+    public Controller(View view, DrawingModel drawingModel) {
         this.view = view;
-        
-        this.drawingModel=drawingModel;
+
+        this.drawingModel = drawingModel;
         view.setSize(600, 400);
-        
+
         // Set Listener
+        setupDrawingPanelListeners();
+
+        //Create Panels
+        view.addPanel(view.getEastPanel(), BorderLayout.EAST);
+        view.addPanel(view.getWestPanel(), BorderLayout.WEST);
+        view.addPanel(view.getSouthPanel(), BorderLayout.SOUTH);
+
+        //Create Colour Buttons
+        view.addButton(view.getWestPanel(), "BLACK", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "WHITE", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "RED", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "BLUE", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "GREEN", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "ORANGE", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "YELLOW", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "CYAN", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "MAGENTA", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "PINK", BorderLayout.WEST, view.getMedBtnSize(), this);
+        view.addButton(view.getWestPanel(), "GRAY", BorderLayout.WEST, view.getMedBtnSize(), this);
+        
+        
+        
+        
+        
+
+        //Create Thickness Buttons
+        view.addButton(view.getEastPanel(), "THIN", BorderLayout.EAST, view.getSmallBtnSize(), this);
+        view.addButton(view.getEastPanel(), "MEDIUM", BorderLayout.EAST, view.getMedBtnSize(), this);
+        view.addButton(view.getEastPanel(), "THICK", BorderLayout.EAST, view.getLargeBtnSize(), this);
+
+        //Create Save/Load Options
+        view.addButton(view.getSouthPanel(), "NEW", BorderLayout.NORTH, view.getMedBtnSize(), this);
+        view.addButton(view.getSouthPanel(), "SAVE", BorderLayout.NORTH, view.getMedBtnSize(), this);
+        view.addButton(view.getSouthPanel(), "OPEN", BorderLayout.NORTH, view.getMedBtnSize(), this);
+        
+
+        //Set Panel Colours
+        //        add(view.getScrollPane());
+        // Show Screen
+        view.setVisible(true);
+        view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public Controller() {
+    }
+
+    private void setupDrawingPanelListeners() {
         view.getDrawingPanel().addMouseListener(this);
         view.getDrawingPanel().addMouseMotionListener(this);
         view.getDrawingPanel().addComponentListener(this);
         view.addWindowListener(this);
         view.addWindowFocusListener(this);
+        //view.getDrawingPanel().setBackground(Color.WHITE);
+    }
 
-        //Create Panels
-        
-        view.addSidePanel(view.getEastPanel(), BorderLayout.EAST);
-        view.addSidePanel(view.getWestPanel(), BorderLayout.WEST);
-        view.addSidePanel(view.getSouthPanel(), BorderLayout.SOUTH);
-        
-        //Create Colour Buttons
-        view.addButton(view.getWestPanel(), "RED", BorderLayout.WEST,view.getMedBtnSize(), this);
-        view.addButton(view.getWestPanel(), "BLUE", BorderLayout.WEST,view.getMedBtnSize(), this);
-        view.addButton(view.getWestPanel(), "GREEN", BorderLayout.WEST,view.getMedBtnSize(), this);
-        view.addButton(view.getWestPanel(), "BLACK", BorderLayout.WEST,view.getMedBtnSize(), this);
-        
-        //Create Thickness Buttons
-        view.addButton(view.getEastPanel(), "THIN", BorderLayout.EAST,view.getSmallBtnSize(), this);
-        view.addButton(view.getEastPanel(), "MEDIUM", BorderLayout.EAST,view.getMedBtnSize(), this);
-        view.addButton(view.getEastPanel(), "THICK", BorderLayout.EAST,view.getLargeBtnSize(),this);
-        
-        //Create Save/Load Options
-        view.addButton(view.getInnerPanel(), "SAVE", BorderLayout.WEST,view.getMedBtnSize(), this);
-        view.addButton(view.getInnerPanel(), "OPEN", BorderLayout.WEST,view.getMedBtnSize(), this);
-        view.addTextArea(view.getTextArea(), view.getInnerPanel(), BorderLayout.EAST);
-        //Set Panel Colours
-        view.getDrawingPanel().setBackground(Color.LIGHT_GRAY);
-//        add(view.getScrollPane());
-        // Show Screen
-        view.setVisible(true);
-        view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    public Controller() {
-    }
-    
-    private void redraw(){
-        for(int i=0;i<this.drawingModel.getNumberLines();i++){
-            stroke=drawingModel.getLine(i);
-            
-            
-            view.drawPoints(stroke.getPoints(),stroke.getColour(),stroke.getThickness());
+   
+
+    private void redraw() {
+        for (int i = 0; i < this.drawingModel.getNumberLines(); i++) {
+            stroke = drawingModel.getLine(i);
+
+            view.drawPoints(stroke.getPoints(), stroke.getColour(), stroke.getThickness());
         }
-        
+
         //view.drawPoints(stroke.getPoints(),this.currentColour,this.currentThickness);
-    }
-    @Override
-    public void mouseClicked(MouseEvent e) {
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        this.stroke=new LineStrokeModel(this.currentColour,this.currentThickness);
+        this.stroke = new LineStrokeModel(this.currentColour, this.currentThickness);
         Point pt = e.getPoint();
         view.setCurrentPosition(pt);
         stroke.addPoint(pt);
@@ -117,64 +145,49 @@ implements MouseListener, MouseMotionListener,
     @Override
     public void mouseReleased(MouseEvent e) {
         Point pt = e.getPoint();
-        view.addPoint(pt,this.currentColour,this.currentThickness);
+        view.addPoint(pt, this.currentColour, this.currentThickness);
         stroke.addPoint(pt);
         this.drawingModel.addLine(stroke);
-        
-    }
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
         Point pt = e.getPoint();
-        view.addPoint(pt, this.currentColour,this.currentThickness);
+        view.addPoint(pt, this.currentColour, this.currentThickness);
         stroke.addPoint(pt);
     }
 
     @Override
-    public void mouseMoved(MouseEvent e) {
-        
+    public void actionPerformed(ActionEvent e) {
+        Color colour = checkColour(e.getActionCommand());
+        if (colour != null) {
+            this.currentColour = colour;
+
+        } else {
+            switch (e.getActionCommand()) {
+                case "THIN":
+                    this.currentThickness = 1;
+                    break;
+                case "MEDIUM":
+                    this.currentThickness = 5;
+                    break;
+                case "THICK":
+                    this.currentThickness = 10;
+                    break;
+                case "SAVE":
+                    saveDrawing();
+                    break;
+                case "OPEN":
+                    open();
+                    break;
+                case "NEW":
+                    newDrawing();
+            }
+        }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-         Color  colour = checkColour(e.getActionCommand());
-         if(colour!=null){
-            this.currentColour=colour;
-            view.getTextArea().append("Colour changed: "+colour.toString()+"\n");
-         }
-         else{
-             switch(e.getActionCommand()){
-                 case "THIN":
-                     this.currentThickness=1;
-                     view.getTextArea().append("Thickness changed: THIN\n");
-                     break;
-                 case "MEDIUM":
-                     this.currentThickness=5;
-                     view.getTextArea().append("Thickness changed: MEDIUM\n");
-                     break;
-                 case "THICK":
-                     this.currentThickness=10;
-                     view.getTextArea().append("Thickness changed: THICK\n");
-                     break;
-                 case "SAVE":
-                     save();
-                     break;
-                 case "OPEN":
-                     open();
-                     break;
-             }
-         }
-    }
-    private Color checkColour(String name){
+    private Color checkColour(String name) {
         Color colour;
         try {
             Field field = Class.forName("java.awt.Color").getField(name);
@@ -185,42 +198,105 @@ implements MouseListener, MouseMotionListener,
         }
         return colour;
     }
+    private void newDrawing(){
+        this.drawingModel = new DrawingModel();
+        view.resetDrawingPanel(view.getGraphics());
+    }
+    
+     public void saveDrawing() {
+
+        JFileChooser fc = new JFileChooser(System.clearProperty("user.home/Document"));
+        fc.setFileFilter(new DrawingFileFilter(".draw", "MyDrawing File"));
+        int result = fc.showSaveDialog(null);
+
+        switch (result) {
+            case JFileChooser.APPROVE_OPTION:
+
+                try {
+                    FileOutputStream outfs = null;
+
+                    try {
+
+                        outfs = new FileOutputStream(fc.getSelectedFile().getAbsoluteFile() + ".draw");
+                        ObjectOutputStream outStream = new ObjectOutputStream(outfs);
+                        outStream.writeObject(this.drawingModel);
+
+                    } catch (FileNotFoundException ex) {
+
+                    } catch (IOException ex) {
+
+                    } finally {
+                        try {
+                            outfs.close();
+                        } catch (IOException ex) {
+
+                        }
+                    }
+                } catch (Exception ex) {
+                    //TODO catch exception message
+                }
+                break;
+            case JFileChooser.CANCEL_OPTION:
+                //TODO CANCEL SAVE FILE CODE 
+                break;
+            case JFileChooser.ERROR_OPTION:
+                //TODO handle SAVE error
+                break;
+        }
+    }
+
+    public void open() {
+
+        JFileChooser fc = new JFileChooser(System.clearProperty("user.home/Document"));
+        fc.setFileFilter(new DrawingFileFilter(".draw", "MyDrawing File"));
+        int result = fc.showOpenDialog(null);
+        switch (result) {
+            case JFileChooser.APPROVE_OPTION:
+                try {
+                    // ArrayList<LineStrokeModel> loadedDrawing=null;
+                    FileInputStream infs = new FileInputStream(fc.getSelectedFile().getAbsoluteFile());
+
+                    ObjectInputStream inputStream = new ObjectInputStream(infs);
+                    this.drawingModel = null;
+                    this.drawingModel = (DrawingModel) inputStream.readObject();
+                    newDrawing();
+                    redraw();
+
+                } catch (Exception ex) {
+
+                }
+
+                //TODO DESIRIALIZE
+                //this.drawingModel = (ArrayList<DrawingModel>)inputStream.readObject();
+                break;
+            case JFileChooser.CANCEL_OPTION:
+                //TODO cancel OPEN FILE code 
+                break;
+            case JFileChooser.ERROR_OPTION:
+                //TODO handle OPEN error
+                break;
+        }
+    }
     @Override
     public void componentResized(ComponentEvent e) {
         redraw();
-        System.out.println("I'm Resized");
-    }
 
-    @Override
-    public void componentMoved(ComponentEvent e) {
-        
     }
 
     @Override
     public void componentShown(ComponentEvent e) {
-        
-        
+        redraw();
     }
 
     @Override
     public void componentHidden(ComponentEvent e) {
         redraw();
-       
+
     }
 
     @Override
     public void windowOpened(WindowEvent e) {
-        
-    }
-
-    @Override
-    public void windowClosing(WindowEvent e) {
-        
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-       
+        redraw();
     }
 
     @Override
@@ -230,13 +306,13 @@ implements MouseListener, MouseMotionListener,
 
     @Override
     public void windowDeiconified(WindowEvent e) {
-       redraw();
+        redraw();
     }
 
     @Override
     public void windowActivated(WindowEvent e) {
         redraw();
-        
+
     }
 
     @Override
@@ -247,7 +323,7 @@ implements MouseListener, MouseMotionListener,
     @Override
     public void windowGainedFocus(WindowEvent e) {
         redraw();
-         
+
     }
 
     @Override
@@ -260,28 +336,37 @@ implements MouseListener, MouseMotionListener,
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void save(){
-        
+    @Override
+    public void mouseEntered(MouseEvent e) {
     }
-    public void open(){
-        FileInputStream inputStream = null;
-        
-        JFileChooser fc = new JFileChooser(System.clearProperty("user.home"));
-        int result = fc.showOpenDialog(null);
-        switch(result){
-            case JFileChooser.APPROVE_OPTION:
-                
-                //TODO DESIRIALIZE
-                //this.drawingModel = (ArrayList<DrawingModel>)inputStream.readObject();
-                break;
-            case JFileChooser.CANCEL_OPTION:
-                //TODO CANCEL OPEN FILE CODE 
-                break;
-            case JFileChooser.ERROR_OPTION:
-                //TODO handle open error
-                break;
-        }
-        //TODO OPEN SERIALIZATION CODE HERE
-        //TODO store SERIALIZED Data
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
 }
