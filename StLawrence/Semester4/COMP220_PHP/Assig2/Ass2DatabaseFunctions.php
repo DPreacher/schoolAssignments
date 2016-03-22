026b076c054e1127950d4f0e2d07e786829f4fce
DPreacher <DPreacher@users.noreply.github.com>
2016-03-22 09:24:21 -0400

    Update Ass2DatabaseFunctions.php

--- StLawrence/Semester4/COMP220_PHP/Assig2/Ass2DatabaseFunctions.php
+++ StLawrence/Semester4/COMP220_PHP/Assig2/Ass2DatabaseFunctions.php
@@ -1,120 +1,117 @@
+ 
 <?php
-	echo "
-	<!doctype html>
-	<html lang = \"en\">
-	   <head>
-		 <meta charset = \"UTF-8\">
-		 <title>Testing DB Functions</title>
-	   </head>
-	   <body>";
-
-	require_once("Asst2DatabaseFunctions.php");
-	error_reporting(0);
-	//ini_set("display_errors", "On");
-	
+	$conn=null;
+	function RunSelect($tablename, $wherefield="", $wherevalue="", $orderfield="", $sort="ASC") {
+		global $conn;
+		$sql = "SELECT * FROM $tablename";
 		
-	function test() {
-		OpenConnectionandDatabase();
-		
-		$tableName="testing";
-		
-		$fieldnames=array(
-			0=>"Item", 
-			1=>"City",
-			2=>"DateReceived",
-			3=>"Quantity",
-			4=>"Price"
-		);
-
-		$dataTypes=array(
-			0=>"varchar", 
-			1=>"varchar",
-			2=>"date",
-			3=>"int",
-			4=>"decimal"
-		);
-
-		$sizes=array(
-			0=>25, 
-			1=>10,
-			2=>0,
-			3=>0,
-			4=>4
-		);
-
-		$decimal=array(
-			0=>0, 
-			1=>0,
-			2=>0,
-			3=>0,
-			4=>2
-		);
-		
-		if (CreateTable($tableName,$fieldnames,$dataTypes,$sizes,$decimal))
-			echo "<p>Table Created Successfully</p>";
-		else
-			echo mysqli_error($conn);
-
-		$value=array(
-			0=>"tea", 
-			1=>"Toronto",
-			2=>"2015-05-31",
-			3=>15,
-			4=>6.45
-		);
-
-		$dataTypes=array(
-			0=>"varchar", 
-			1=>"varchar",
-			2=>"date",
-			3=>"int",
-			4=>"decimal"
-		);	
-
-		if (InsertIntoTable($tableName, $value, $dataTypes))
-			echo "<p>Data Inserted Successfully</p>";
-		else
-			echo mysqli_error($conn);
-
-		$value=array(
-			0=>"Milk", 
-			1=>"Kingston",
-			2=>"2014-06-17",
-			3=>100,
-			4=>0.99
-		);
-
-		$dataTypes=array(
-			0=>"varchar", 
-			1=>"varchar",
-			2=>"date",
-			3=>"int",
-			4=>"decimal"
-		);
-		
-		if(InsertIntoTable($tableName, $value, $dataTypes)) 
-			echo "<p>Data Inserted Successfully</p>";
-		else
-			echo mysqli_error($conn);
-		
-		$result = RunSelect($tableName, "Item");
-
-		if ($result) {
-			echo "<ul>";
-			while($row = GetOneRow($result)) {
-				echo "<li>";
-				print_r($row);
-				echo "</li>";
-			}
-			echo "</ul>";
+		if ($wherefield != "" && isset($wherevalue) && $wherevalue != "") {
+			if (gettype($wherevalue) == "string" || gettype($wherevalue) == "date")
+				$wherevalue = "'" . $wherevalue . "'";
+			$sql = $sql . " WHERE $wherefield = $wherevalue ";
 		}
-		else
-			echo mysqli_error($conn);
-		CloseConnection();
-	}
 		
-  	test();
-    
-	echo "</body>
-         </html>";
+		if ($orderfield != "")
+			$sql = $sql . " ORDER BY $orderfield $sort";
+
+		$result = mysqli_query($conn, $sql);
+	
+		if (!$result)
+			echo mysqli_error($conn);
+	
+		//echo $sql;
+
+		return $result;
+	}
+	
+	function CreateTable($tablename, $fieldnames, $datatypes, $sizes, $decimal) {
+		global $conn;
+		mysqli_query($conn, "DROP TABLE IF EXISTS $tablename");
+		
+		$sql = "CREATE TABLE $tablename (";
+		
+		for ($i = 0; $i < count($fieldnames); $i++) {
+			$sql = $sql . $fieldnames[$i] . " " . $datatypes[$i];
+			if ($datatypes[$i] == "varchar")
+				$sql = $sql . "(" . $sizes[$i] . ")";
+			else if ($datatypes[$i] == "decimal")
+				$sql = $sql . "(" . $sizes[$i] . ", " . $decimal[$i] . ")";
+			
+			if ($i < count($fieldnames) - 1)
+				$sql = $sql . ",";
+		}
+		
+		$sql = $sql . ")";
+		
+		$result = mysqli_query($conn, $sql);
+		
+		//echo $sql;
+
+		if (!$result)
+			$result = mysqli_error($conn);
+
+		return $result != false;
+	}
+	
+	function InsertIntoTable($tablename, $values, $datatypes) {
+		global $conn;		
+		$sqlRecord = "INSERT INTO $tablename VALUES (";
+		
+		for($ctr=0;$ctr<count($values);$ctr++) {
+			
+			if ($datatypes[$ctr]=="varchar" || $datatypes[$ctr]=="date") {
+				$sqlRecord = $sqlRecord . "'" . $values[$ctr] . "'";
+			}
+			else {
+				$sqlRecord = $sqlRecord . $values[$ctr];
+
+				/*
+				if($datatypes[$ctr]=="int"||$datatypes[$ctr]=="decimal") {
+					if($values[$ctr]==null) {
+						$sqlRecord = 0;
+					}
+				}
+				*/				
+			}
+			if($ctr < count($values) - 1)
+				$sqlRecord=$sqlRecord . ",";
+		}
+	
+		$sqlRecord = $sqlRecord . ")";
+
+		//echo $sqlRecord;
+
+		$result = mysqli_query($conn, $sqlRecord);
+
+		if (!$result)
+			echo mysqli_error($conn);
+
+		return $result != false;
+	}
+
+	
+	function OpenConnectionandDatabase(){
+		global $conn;
+		$connAquired = false;
+		$databaseExists = false;
+		$conn = mysqli_connect("localhost","root","mysql","test");
+		if($conn) 
+			$connAquired=true;
+		else
+			print_r(mysqli_connect_error());
+		$result=mysqli_query($conn,"use test");
+		if($result)
+			$databaseExists=true;
+		return $connAquired && $databaseExists;
+	}
+	
+	function GetOneRow($Resultset){
+		$row = mysqli_fetch_array($Resultset);
+		return $row;
+	}
+	
+	function CloseConnection(){
+		global $conn;
+		mysqli_close($conn);
+	}
 ?>
