2f1b35617b94109bb842f8c4c6f91daa7367906f
DPreacher <DPreacher@users.noreply.github.com>
2017-02-13 10:49:41 -0500

    Create Line.java

---
+++ StLawrence/COMP208_Android/Assignment/Assignment2/Line.java
@@ -0,0 +1,15 @@
+package ca.on.sl.comp208.paintgame;
+
+import android.graphics.Point;
+
+/**
+ * Created by dingram20 on 1/30/2017.
+ */
+public class Line {
+    public Point start;
+    public Point end;
+    public Line(){
+        start = new Point(0,0);
+        end = new Point(0,0);
+    }
+}
